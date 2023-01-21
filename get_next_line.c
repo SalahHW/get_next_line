9249c13917592821693338420b5abb66451ccebf
Salah <salah.bouheniche@gmail.com>
2023-01-21 16:42:57 +0100

    add some function to manage read behavior

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 17:11:31 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/21 01:52:14 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/21 16:41:59 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,23 @@ char	*get_next_line(int fd)
 	char		buffer[BUFFER_SIZE + 1];
 	int			read_state;
 
-	if (fd >= 0)
+	if (fd >= 0 && BUFFER_SIZE > 0)
 	{
 		read_state = 1;
 		while (read_state > 0)
 		{
 			read_state = read(fd, buffer, BUFFER_SIZE);
-			stash = malloc(sizeof(char)* read_state + 1);
+			if (read_state == 0)
+			{
+				return (NULL);
+			}
+			if (!stash)
+				stash = malloc(sizeof(char) * read_state + 1);
 			stash = join_strings(stash, buffer);
 			if (find_cr(stash))
 			{
 				line = extract_line(stash);
+				stash = keep_remainder(stash);
 				return (line);
 			}
 		}
@@ -56,4 +62,26 @@ char	*extract_line(char *str)
 	line[i] = '\n';
 	line[i + 1] = '\0';
 	return (line);
+}
+
+char	*keep_remainder(char *str)
+{
+	char	*new_stash;
+	int		i;
+
+	i = 0;
+	str = find_cr(str);
+	str++;
+	while (str[i])
+	{
+		i++;
+	}
+	new_stash = malloc(sizeof(char) * i + 1);
+	i = 0;
+	while (str[i])
+	{
+		new_stash[i] = str[i];
+		i++;
+	}
+	return (new_stash);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 15:51:55 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/01/21 01:16:50 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/01/21 15:36:35 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 6
 # endif
 
 char	*get_next_line(int fd);
 char	*join_strings(char *dst, char *src);
 char	*find_cr(const char *s);
 char	*extract_line(char *str);
+char	*keep_remainder(char *str);
 int		get_length(char *str);
 
 #endif
